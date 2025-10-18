e59292afce0701156b6aebaa4310d4d9298da2a9
Husseinghosn <95096809+Husseinghosn@users.noreply.github.com>
2025-10-18 17:47:14 -0400

    changed head color to green

--- game.py
+++ game.py
@@ -21,6 +21,9 @@ RED = (200,0,0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 BLACK = (0,0,0)
+HEAD_COLOR = (0, 255, 0)    
+HEAD_COLOR2 = (0, 200, 0)   # new: head inner color (darker green)
+
 
 BLOCK_SIZE = 20
 SPEED = 20
@@ -195,9 +198,15 @@ class SnakeGame:
     def _update_ui(self):
         self.display.fill(BLACK)
         
-        for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+        for i, pt in enumerate(self.snake):
+            if i == 0:
+                outer = HEAD_COLOR
+                inner = HEAD_COLOR2
+            else:
+                outer = BLUE1
+                inner = BLUE2
+            pygame.draw.rect(self.display, outer, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, inner, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
             
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         
